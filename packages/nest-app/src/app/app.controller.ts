e7922d5061962639a1ce5d7f29a6451ee3770a48
claudealdric <cvaldric@gmail.com>
2023-03-18 20:13:05 -0400

    Create a route that determines if a number is odd

--- packages/nest-app/src/app/app.controller.ts
+++ packages/nest-app/src/app/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 import { AppService } from './app.service';
 
@@ -9,5 +9,10 @@ export class AppController {
   @Get()
   sayHello() {
     return this.appService.sayHello();
+  }
+
+  @Get(':number')
+  isOdd(@Param('number') number: string) {
+    return this.appService.isOdd(number);
   }
 }
--- packages/nest-app/src/app/app.service.ts
+++ packages/nest-app/src/app/app.service.ts
@@ -1,7 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+
+import { isOdd } from '@nx-integrated/is-odd';
 
 @Injectable()
 export class AppService {
+  isOdd(input: string) {
+    const parsedNumber = Number(input);
+
+    if (Number.isNaN(parsedNumber)) {
+      throw new HttpException(
+        `${input} is not a valid number`,
+        HttpStatus.BAD_REQUEST
+      );
+    }
+
+    return `${parsedNumber} is ${isOdd(parsedNumber) ? '' : 'not'} odd.`;
+  }
+
   sayHello(): string {
     const greeting = this.convertFirstLetterToUppercase(process.env.GREETING);
     const name = process.env.NAME;
